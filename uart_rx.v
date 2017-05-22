231666f3c98e5b1e8451e038c434611e87aa0198
prohorov_leonid@mail.ru <prohorov_leonid@mail.ru>
2017-05-23 01:10:11 +0300

    Fixed RX and TX logic

--- uart_rx.v
+++ uart_rx.v
@@ -4,56 +4,68 @@ parameter [1:0]STATE_RX_START	=2'b01;
 parameter [1:0]STATE_RX_STOP	=2'b10;
 
 reg [1:0]state;
-reg [8:0]data;
+reg [7:0]data;
 reg [1:0]clk_count;
 reg [4:0]bit_count;
 
 always @(posedge clk)
 begin
-    if (rst)
-    begin
-        state <= STATE_IDLE;
-        data <= 8'h00;
-        out <= 8'h00;
-        recv_finish <= 0;
-	clk_count <= 0;
-	bit_count <= 0;
-    end
-    else
-    begin
-	case (state)
-	    STATE_IDLE:
+	if (rst)
+	begin
+		state <= STATE_IDLE;
+		data <= 8'h00;
+		out <= 8'h00;
+		recv_finish <= 0;
+		clk_count <= 0;
+		bit_count <= 0;
+	end
+	else
+	begin
+		case (state)
+		STATE_IDLE:
 		begin
-		    if (rx == 0)
-		    begin
-			clk_count <= 0;
-			data <= 0;
-			bit_count <= 0;
-			state <= STATE_RX_START;
+			if (rx == 0)
+			begin
+				clk_count <= 0;
+				data <= 0;
+				bit_count <= 0;
+				state <= STATE_RX_START;
+			end
+			else
+			begin
+				if (clk_count < 2'b11)
+					clk_count <= clk_count + 2'b01;
+				else
+				begin
+					clk_count <= 2'b00;
+					recv_finish <= 0;
+				end
+			 end
+		end
+		STATE_RX_START:
+		begin
+			if (clk_count < 2'b01)
+				clk_count <= clk_count + 2'b01;
+			else
+			begin
+				clk_count <= 2'b00;
+				if (bit_count < 4'h8)
+				begin
+					data <= data>>1;
+					data[7] <= rx;
+					bit_count <= bit_count + 4'h1;
+				end
+				else
+					if (rx == 1)
+					begin
+						out <= data;
+						recv_finish = 1;
+						state <= STATE_IDLE;
+					end
 		    end
 		end
-	    STATE_RX_START:
-		begin
-		    if (clk_count < 2'b01)
-			clk_count <= clk_count + 2'b01;
-		    else
-		    begin
-                        clk_count <= 2'b00;
-                        if (bit_count < 4'h8)
-                        begin
-                            data <= data>>1;
-                            data[7] <= rx;
-                            bit_count <= bit_count + 4'h1;
-                        end
-                        else
-                        begin
-                            out <= data;
-                            recv_finish = 1;
-                        end
-		    end
-		end
-        endcase
-    end
+		endcase
+	end
 end
 
 endmodule
--- uart_tx.v
+++ uart_tx.v
@@ -1,5 +1,5 @@
 module uart_tx(input wire clk, input wire rst, input wire tx_start, input wire [7:0]tx_in, output reg tx_out, output reg tx_finish);
-parameter [1:0]STATE_IDLE	=2'b00;
+parameter [1:0]STATE_IDLE	= 2'b00;
 parameter [1:0]STATE_TX_START	=2'b01;
 parameter [1:0]STATE_TX_DATA	=2'b10;
 parameter [1:0]STATE_TX_STOP	=2'b11;
@@ -11,65 +11,58 @@ reg [4:0]bit_count;
 
 always @(posedge clk)
 begin
-    if (rst)
-    begin
-        state <= STATE_IDLE;
-        data <= 8'h00;
-        tx_finish <= 0;
-	clk_count <= 0;
-	bit_count <= 0;
-	tx_out <= 1;
-    end
-    else
-    begin
-        if (tx_start)
-        begin
-            case (state)
-                STATE_IDLE:
-                begin
-                    data <= tx_in;
-                    state <= STATE_TX_START;
-                    tx_out <= 0;
-                    bit_count <= 0;
-                    clk_count <= 0;
-                end
-                STATE_TX_START:
-                begin
-                    if (clk_count == 2'b01)
-                    begin
-                        state <= STATE_TX_DATA;
-                        tx_out <= data[0];
-                        data <= data>>1;
-                        bit_count <= 1;
-                    end
-                end
-                STATE_TX_DATA:
-                begin
-                    if (clk_count == 2'b01)
-                    begin
-                        if (bit_count<8)
-                        begin
-                            tx_out <= data[0];
-                            data <= data>>1;
-                            bit_count <= bit_count +1;
-                        end
-                        else
-                        begin
-                            state <= STATE_TX_STOP;
-                            tx_out <= 1;
-                        end
-                    end
-                end
-                STATE_TX_STOP:
-                    if (clk_count == 2'b01)
-                    begin
-                        state <= STATE_IDLE;
-                        tx_finish <= 1;
-                    end
-            endcase
-        end
+	if (rst)
+	begin
+		state <= STATE_IDLE;
+		data <= 8'h00;
+		tx_finish <= 0;
+		clk_count <= 0;
+		bit_count <= 0;
+		tx_out <= 1;
+	end
+	else
+	begin
+		case (state)
+		STATE_IDLE:
+			if (tx_start)
+			begin
+				data <= tx_in;
+				state <= STATE_TX_START;
+				tx_out <= 0;
+				bit_count <= 0;
+				clk_count <= 0;
+			end
+		STATE_TX_START:
+			if (clk_count == 2'b01)
+			begin
+				state <= STATE_TX_DATA;
+				tx_out <= data[0];
+				data <= data>>1;
+				bit_count <= 1;
+			end
+		STATE_TX_DATA:
+			if (clk_count == 2'b01)
+				if (bit_count<8)
+				begin
+					tx_out <= data[0];
+					data <= data>>1;
+					bit_count <= bit_count + 5'h1;
+				end
+				else
+				begin
+					state <= STATE_TX_STOP;
+					tx_out <= 1;
+				end
+		STATE_TX_STOP:
+			if (clk_count == 2'b01)
+			begin
+				state <= STATE_IDLE;
+				tx_finish <= 1;
+			end
+		endcase
     end
     
+	 //divide clock by 2
     if (state != STATE_IDLE)
         if (clk_count<1)
             clk_count <= clk_count + 2'b01;
